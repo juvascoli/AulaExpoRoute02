18eacc974672fb1c73a35e83089bc8594944fbb9
juvascoli <jvascoli6@gmail.com>
2025-04-15 11:36:19 -0300

    second commit

---
+++ app/(drawer)/index.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from "react-native";
+import {Link} from "expo-router"
+
+export default function DrawerHome(){
+    return(
+        <View>
+            <Text>
+                Bem vindo ao meu drawer
+            </Text>
+        </View>
+    )
+}
--- app/login.tsx
+++ app/login.tsx
@@ -1,7 +1,12 @@
 import {Text} from "react-native"
 
+import {Link} from "expo-router"
+
 export default function TelaLogin(){
     return(
+       <>
         <Text>Tela Login</Text>
+        <Link href = "/(drawer)"> Ir para o drawer</Link>
+        </>
     )
 }
